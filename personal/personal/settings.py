6eda4f9c1ab1805522fa7cc89c0ceec98ac11944
gestalt8003 <cmcummings2003@gmail.com>
2018-11-11 20:58:53 -0600

    update

--- personal/personal/settings.py
+++ personal/personal/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'portfolio.apps.PortfolioConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
