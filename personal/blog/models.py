4a1861f9dbaa2df20aae1d1eabc1042a9a501003
gestalt8003 <cmcummings2003@gmail.com>
2018-11-12 15:27:28 -0600

    update

--- personal/blog/models.py
+++ personal/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Tag(models.Model):
+    tag = models.CharField(max_length=30)
+    description = models.CharField(max_length=50, blank=True)
+
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    tags = models.ManyToManyField(Tag)
+    date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
